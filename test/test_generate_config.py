5970e5f8b6f76c42f453c8afc3e9e4ebb350e813
Kat Steinke <kati.steinke@gmail.com>
2020-07-13 14:17:40 +0200

    restructure to allow easy tests

---
+++ test/test_generate_config.py
@@ -0,0 +1,14 @@
+import unittest
+
+from pathlib import Path
+
+import magician.generate_camisim_config as camiconf
+
+class TestLineCount(unittest.TestCase):
+    def test_file_with_lines(self):
+        line_path = Path('test/data/genomefile_2line')
+        assert camiconf.get_file_length(line_path) == 2
+
+    def test_blank_file(self):
+        blank_path = Path('test/data/blank_file')
+        assert camiconf.get_file_length(blank_path) == 0
