7667588ea174c85867eb509d4a96b32dde5c31be
Kat Steinke <kati.steinke@gmail.com>
2020-07-15 13:35:18 +0200

    extract_camisim_data: add option for generating abundance file

--- magician/extract_camisim_data.py
+++ magician/extract_camisim_data.py
@@ -2,18 +2,20 @@
 
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import List
+from typing import Optional, List
 
 from Bio import SeqIO
 
 
-def create_camisim_files(genbanks: List[str]):
+def create_camisim_files(genbanks: List[str], make_abundance: Optional[bool]=False):
     """Generate metadata and ID mapping files for CAMISIM and convert genbanks to FASTA.
         Arguments:
-            genbanks: paths of the genbank files to use for the CAMISIM run
+            genbanks:       paths of the genbank files to use for the CAMISIM run
+            make_abundance: flag for generating a tsv file specifying even abundance for genomes
         Returns:
             A tab-separated metadata file containing genome ID, OTU, NCBI taxid and novelty category,
-            a fasta file with the sequence, and
+            a fasta file with the sequence,
+            a tab-separated file listing genome ID and abundance, and
             a tab-separated file listing the genome ID and path of the fasta file
             for each genbank.
     """
@@ -22,6 +24,10 @@ def create_camisim_files(genbanks: List[str]):
     metadata = ["genome_ID\tOTU\tNCBI_ID\tnovelty_category"]
     # initialize id_to_genome_file to blank
     id_to_genome = []
+    # if abundance file wanted, initialize that
+    if make_abundance:
+        common_abundance = 1 / len(genbanks)
+        id_to_abundance = []
     # initialize OTU count to 1 as we want all to be included
     otu_count = 1
     # check if dirs exist
@@ -50,20 +56,29 @@ def create_camisim_files(genbanks: List[str]):
         SeqIO.convert(genbank, "genbank", fasta_path, "fasta")
         # id to file line
         id_to_genome.append("{}\t{}".format(record_id, fasta_path))
+        # if abundance is to be generated:
+        if make_abundance:
+            id_to_abundance.append("{}\t{}".format(record_id, common_abundance))
 
-    # write metadata file - TODO: separate dir
+    # write metadata file
     with open(Path("camisim_configfiles", "metadata"), "w") as meta_file:
         meta_file.write("\n".join(metadata))
     # write id_to_genome_file
     with open(Path("camisim_configfiles","id_to_genome_file"), "w") as id_file:
         id_file.write("\n".join(id_to_genome))
+    if make_abundance:
+        with open(Path("camisim_configfiles", "id_to_distributions"), "w") as abundance_file:
+            abundance_file.write("\n".join(id_to_abundance))
 
 if __name__ == "__main__":
     # input: list of files
     parser = ArgumentParser(
         description="Prepare CAMISIM metadata and id to file mapping files and create FASTA files from gbks")
     parser.add_argument("gbk_files", nargs="+", help="Genbank files for CAMISIM run")
+    parser.add_argument("--even_abundance", action="store_true",
+                        help="Automatically generate abundance file with even distribution")
     # TODO: add default names for metadata etc files
     args = parser.parse_args()
     gbk_files = args.gbk_files
-    create_camisim_files(gbk_files)
+    abundance = args.even_abundance
+    create_camisim_files(gbk_files, abundance)
