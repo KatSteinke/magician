d7c584fa6739f27e6d78004f9bb867290adfc19b
Kat Steinke <kati.steinke@gmail.com>
2020-11-25 12:00:04 +0100

    Snakefile: add option for insert size

--- run_magician.py
+++ run_magician.py
@@ -6,23 +6,28 @@ from argparse import ArgumentParser
 from typing import Optional
 
 
-def run_snakemake(target: str, profile_type: Optional[str]="mbarc", profile_base: Optional[str]="",
-                  readlength: Optional[str]="", *snake_params) -> None:
+def run_snakemake(target: str, profile_type: Optional[str] = "mbarc", profile_base: Optional[str] = "",
+                  readlength: Optional[str] = "", insert_size: Optional[int] = 270,
+                  *snake_params) -> None:
     """Runs a Snakemake command with optional configuration parameters.
     Arguments:
         target:         rule or output file for Snakemake
         profile_type:   type of ART error profile to use for CAMISIM
         profile_base:   path to own ART error profile for CAMISIM
         readlength:     read length used for own ART error profile
+        insert_size:    mean insert size to use for simulation (default: 270)
     """
     # TODO: make testable
     # check all elements of the command
-    for input_param in [target, profile_type, profile_base, readlength]:
+    for input_param in [target, profile_type, profile_base, readlength, insert_size]:
         bad_chars = re.search(r"""[^a-zA-Z0-9"'./_\- ]""", input_param)
         if bad_chars:
             raise ValueError("Invalid character(s) in one of the parameters: {}.".format(bad_chars.group(0)))
 
     # sanity check parameters
+    if insert_size <= 0:
+        raise ValueError("Insert size needs to be above 0.")
+
     if profile_type == "own" and not (profile_base and readlength):
         raise ValueError("Both name of the custom error profile and read length of the error profile must be given when using own profiles.")
 
@@ -39,7 +44,8 @@ def run_snakemake(target: str, profile_type: Optional[str]="mbarc", profile_base
     snake_path = pathlib.Path(__file__).resolve().parent / "snakefiles" / "Snakefile"
 
     subprocess.run(["snakemake", target, "-s", snake_path, "--config", 'profile_type="{}"'.format(profile_type),
-                    'profile_name="{}"'.format(profile_base), 'readlength="{}"'.format(readlength), *snake_params])
+                    'profile_name="{}"'.format(profile_base), 'readlength="{}"'.format(readlength),
+                    'insert_size={}'.format(insert_size), *snake_params])
     # construct string
     #snakemake_cmd = 'snakemake {target} -s {snakefile} --config profile_type="{profile_type}" \
     #profile_name="{profile_base}" readlength="{read_length}"'.format(target=target, snakefile=snake_path,
@@ -59,13 +65,16 @@ if __name__ == "__main__":
                          required with 'own' error profile""", default="")
     parser.add_argument("--profile_readlength", action="store",
                         help="Read length of custom error profile; required with 'own' error profile", default="")
+    parser.add_argument("--insert_size", action="store", type=int, default=270,
+                        help="Mean insert size for read simulation (default: 270)")
     parser.add_argument("--snake_flags", nargs='*', help="Flags to be passed to snakemake, enclosed in quotes")
     args = parser.parse_args()
     target_result = args.target
     profiletype = args.profile_type
     profilename = args.profile_name
     read_length = args.profile_readlength
+    insert_size = args.insert_size
     snake_flags = args.snake_flags[0].split()
 
-    run_snakemake(target_result, profiletype, profilename, read_length, *snake_flags)
+    run_snakemake(target_result, profiletype, profilename, read_length, insert_size, *snake_flags)
 
