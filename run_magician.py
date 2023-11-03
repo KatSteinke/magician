151a506d391a76d5c05f4b40b3c77bea6ea59658
Kat Steinke <kati.steinke@gmail.com>
2023-11-03 15:14:20 +0100

    run_magician: make Snakemake command testable

--- run_magician.py
+++ run_magician.py
@@ -3,14 +3,14 @@ import re
 import subprocess
 
 from argparse import ArgumentParser
-from typing import Optional
+from typing import List, Optional
 
-#import subprocrunner
-
-def run_snakemake(target: str, profile_type: Optional[str] = "mbarc", profile_base: Optional[str] = "",
-                  readlength: Optional[str] = "", insert_size: Optional[int] = 270, cluster_cmd: Optional[str] = "",
-                  *snake_params) -> None:
-    """Runs a Snakemake command with optional configuration parameters.
+def get_snake_cmd(target: str, profile_type: Optional[str] = "mbarc", profile_base: Optional[str] = "",
+                  readlength: Optional[str] = "",
+                  insert_size: Optional[int] = 270,
+                  cluster_cmd: Optional[str] = "",
+                  *snake_params) -> List[str]:
+    """Get the Snakemake command with optional configuration parameters.
     Arguments:
         target:         rule or output file for Snakemake
         profile_type:   type of ART error profile to use for CAMISIM
@@ -18,8 +18,8 @@ def run_snakemake(target: str, profile_type: Optional[str] = "mbarc", profile_ba
         readlength:     read length used for own ART error profile
         insert_size:    mean insert size to use for simulation (default: 270)
         cluster_cmd:    command to use for cluster mode
+        snake_params:   parameters to pass to the Snakefile
     """
-    # TODO: make testable
     # check all elements of the command
     for input_param in [target, profile_type, profile_base, readlength, insert_size]:
         bad_chars = re.search(r"""[^a-zA-Z0-9"'./_\- ]""", str(input_param))
@@ -55,17 +55,20 @@ def run_snakemake(target: str, profile_type: Optional[str] = "mbarc", profile_ba
         snakemake_cmd.insert(4, "--cluster")
         snakemake_cmd.insert(5, cluster_cmd)
 
-    subprocess.run(snakemake_cmd)
-    #test_runner = subprocrunner.SubprocessRunner(snakemake_cmd, dry_run=True)
-    #print("command: {}".format(test_runner.command))
-    #subprocess.run(["snakemake", target, "-s", snake_path, "--config", 'profile_type="{}"'.format(profile_type),
-    #                'profile_name="{}"'.format(profile_base), 'readlength="{}"'.format(readlength),
-    #                'insert_size={}'.format(insert_size), *snake_params])
+    return snakemake_cmd
     
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Run MAGICIAN to create a specified file.")
-    parser.add_argument("target", action="store", help="Desired output file or rule")
+    parser = ArgumentParser(description="Run MAGICIAN to simulate MAGs for a specified community"
+                                        " or set of communities.")
+    parser.add_argument("community_file", action="store",
+                        help="File with paths to source genomes, sequence type (plasmid/chromosome)"
+                             " and their desired relative copy number in each community to simulate "
+                             "(one column with organisms' copy numbers per community)")
+    parser.add_argument("--target", action="store",
+                        help="Desired output file or rule "
+                             "(default: MAGs, statistics and summary files for all communities)",
+                        default="all_bin_summaries")
     parser.add_argument("--profile_type", action="store",
                         help="Type of ART error profile to use for CAMISIM: mbarc, hi, mi, hi150, own (default: mbarc)",
                         default="mbarc", choices=['mbarc', 'hi', 'mi', 'hi150', 'own'])
@@ -81,6 +84,7 @@ if __name__ == "__main__":
                         would with snakemake.""")
     parser.add_argument("--snake_flags", nargs='*', help="Flags to be passed to snakemake, enclosed in quotes")
     args = parser.parse_args()
+    community_file = pathlib.Path(args.community_file)
     target_result = args.target
     profiletype = args.profile_type
     profilename = args.profile_name
@@ -89,5 +93,7 @@ if __name__ == "__main__":
     cluster_cmd = args.cluster
     snake_flags = args.snake_flags[0].split()
 
-    run_snakemake(target_result, profiletype, profilename, read_length, insert_size, cluster_cmd, *snake_flags)
+    snake_command = get_snake_cmd(target_result, profiletype, profilename, read_length, insert_size, cluster_cmd,
+                                  *snake_flags)
+    subprocess.run(snake_command)
 
--- test/test_run_magician.py
+++ test/test_run_magician.py
@@ -1,20 +1,68 @@
+import pathlib
 import unittest
 
 import run_magician
 
 
 class TestRunMagician(unittest.TestCase):
+    snake_path = pathlib.Path(__file__).resolve().parent.parent / "snakefiles" / "Snakefile"
+    profile_type = "mbarc"
+    profile_base = ""
+    readlength = ""
+    insert_size = 270
+    cluster_cmd = ""
+    def test_default_command_success(self):
+        """Run Snakemake with default settings."""
+        expected_command = ["snakemake", "all_bin_summaries", "-s", self.snake_path,
+                            "--config", 'profile_type="mbarc"',
+                            'profile_name="False"', 'readlength="False"',
+                            'insert_size=270', "-n"]
+        snake_flags = ["-n"]
+        test_command = run_magician.get_snake_cmd("all_bin_summaries", self.profile_type, self.profile_base,
+                                                  self.readlength, self.insert_size, self.cluster_cmd, *snake_flags)
+        assert test_command == expected_command
+
+
+    def test_run_cluster_command(self):
+        """Run Snakemake in cluster mode"""
+        expected_command = ["snakemake", "all_bin_summaries", "-s", self.snake_path,
+                            "--cluster", "qsub -pe threaded {threads}",
+                            "--config", 'profile_type="mbarc"',
+                            'profile_name="False"', 'readlength="False"',
+                            'insert_size=270', "-n"]
+        snake_flags = ["-n"]
+        cluster_cmd = "qsub -pe threaded {threads}"
+        test_command = run_magician.get_snake_cmd("all_bin_summaries", self.profile_type, self.profile_base,
+                                                  self.readlength, self.insert_size, cluster_cmd,
+                                                  *snake_flags)
+        assert test_command == expected_command
+
+    def test_change_settings(self):
+        """Use non-default settings for various parameters."""
+        expected_command = ["snakemake", "all_bin_summaries", "-s", self.snake_path,
+                            "--config", 'profile_type="mbarc"',
+                            'profile_name="False"', 'readlength="False"',
+                            'insert_size=500', "-n"]
+        snake_flags = ["-n"]
+        insert_size = 500
+        test_command = test_command = run_magician.get_snake_cmd("all_bin_summaries", self.profile_type,
+                                                                 self.profile_base,
+                                                                 self.readlength, insert_size, self.cluster_cmd,
+                                                                 *snake_flags)
+        assert test_command == expected_command
+
     def test_bad_file_name(self):
         bad_result = "test_ä.txt"
-        with self.assertRaisesRegex(ValueError, r"""Arguments can only consist of alphanumeric characters, quote marks, \., \/, \_, \- and space."""):
-            run_magician.run_snakemake(bad_result)
+        with self.assertRaisesRegex(ValueError,
+                                    r"""Arguments can only consist of alphanumeric characters, quote marks, \., \/, \_, \- and space."""):
+            run_magician.get_snake_cmd(bad_result)
 
     def test_missing_profile_info(self):
         result = "test.txt"
         profile = "own"
         with self.assertRaisesRegex(ValueError,
                                     "Both name of the custom error profile and read length of the error profile must be given when using own profiles."):
-            run_magician.run_snakemake(result, profile)
+            run_magician.get_snake_cmd(result, profile)
 
     def test_too_much_info(self):
         result = "test.txt"
@@ -22,4 +70,4 @@ class TestRunMagician(unittest.TestCase):
         extra_name = "TestR"
         with self.assertRaisesRegex(ValueError,
                                     "Name of the error profile and read length can only be specified when using own profiles."):
-            run_magician.run_snakemake(result, profile, extra_name)
+            run_magician.get_snake_cmd(result, profile, extra_name)
